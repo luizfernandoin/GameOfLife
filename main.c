3ee49288d5d8ebc97686e8b8d762bad9124b1872
Luiz Fernando <luizfernandoinfo100@gmail.com>
2024-03-09 08:09:56 -0300

    feat: adicionando regra para frames internos

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 
 
-const int TAMANHO = 20;
+const int TAMANHO = 3;
 
 int randomNumber() {
     return rand() % 2; 
@@ -22,8 +22,8 @@ void fillRandom(int currentGenerationMatrix[TAMANHO][TAMANHO]) {
 }
 
 void printMatrix(int matrix[TAMANHO][TAMANHO]) {
-    system("clear");
-    
+    printf("\n");
+        
     for (int row = 0; row < TAMANHO; row++) {
         for (int col = 0; col < TAMANHO; col++) {
             if (matrix[row][col] == 1) {
@@ -48,20 +48,126 @@ void printMatrix(int matrix[TAMANHO][TAMANHO]) {
 
 // }
 
-// void proximoCenario() {
+int calcularVizinhos(int matrix[TAMANHO][TAMANHO], int row, int col, int totVizinhos, int quadrante) {
+    int totBacterias = 0;
 
-// }
+    switch (totVizinhos) {
+        // case 3:
+        //     switch(quadrante) {
+        //         case 1:
+        //             for (int rowSub = (row - 1); rowSub <= row; rowSub++) {
+        //                 for (int colSub = (col - 1); colSub <= col; colSub++) {
+        //                     if (matrix[rowSub][colSub] == 1) totBacterias++;
+        //                 }
+        //             }
+        //             break;
+        //         case 2:
+        //             for (int rowSub = (row - 1); rowSub <= row; rowSub++) {
+        //                 for (int colSub = col; colSub <= (col+1); colSub++) {
+        //                     if (matrix[rowSub][colSub] == 1) totBacterias++;
+        //                 }
+        //             }
+        //             break;
+        //         case 3:
+        //             for (int rowSub = row; rowSub <= (row+1); rowSub++) {
+        //                 for (int colSub = (col - 1); colSub <= col; colSub++) {
+        //                     if (matrix[rowSub][colSub] == 1) totBacterias++;
+        //                 }
+        //             }
+        //             break;
+        //         case 4:
+        //             for (int rowSub = row; rowSub <= (row+1); rowSub++) {
+        //                 for (int colSub = col; colSub <= (col+1); colSub++) {
+        //                     if (matrix[rowSub][colSub] == 1) totBacterias++;
+        //                 }
+        //             }
+        //             break;
+        //     }
+        //     break;
+        // case 5:
+        //     switch (totVizinhos) {
+        //         case 1:
+        //             for (int rowSub = (row - 1); rowSub <= row; rowSub++) {
+        //                 for (int colSub = (col-1); colSub <= (col+1); colSub++) {
+        //                     if (matrix[rowSub][colSub] == 1) totBacterias++;
+        //                 }
+        //             }
+        //             break;
+        //         case 2:
+        //             for (int rowSub = row; rowSub <= (row+1); rowSub++) {
+        //                 for (int colSub = (col-1); colSub <= (col+1); colSub++) {
+        //                     if (matrix[rowSub][colSub] == 1) totBacterias++;
+        //                 }
+        //             }
+        //             break;
+                
+        //         default:
+        //             break;
+        //     }
+        //     break;
+        case 8:
+            for (int rowSub = (row - 1); rowSub <= (row+1); rowSub++) {
+                for (int colSub = (col - 1); colSub <= (col+1); colSub++) {
+                    if (matrix[rowSub][colSub] == 1 && (rowSub != row || colSub != col)) totBacterias++;
+                }
+            }
+            break;
+    }
+
+    return totBacterias;
+}
+
+int proximaGeracao(int currentGenerationMatrix[TAMANHO][TAMANHO], int nextGenerationMatrix[TAMANHO][TAMANHO]) {
+    int vizinhos = 0;
+    int quadrante = 0;
+
+    for (int row = 0; row < TAMANHO; row++) {
+        for (int col = 0; col < TAMANHO; col++) {
+            if ((row > 0 && row < (TAMANHO - 1)) && (col > 0 && col < (TAMANHO - 1))) {
+                vizinhos = 8;
+            } 
+            // else if ((row == 0 || row == TAMANHO - 1) || (col == 0 || col == TAMANHO - 1)) {
+            //     vizinhos = 5;
+            //     quadrante = (row == 0) ? 2 : 1;
+            // } else {
+            //     vizinhos = 3;
+            //     if (row == 0 && col == 0) quadrante = 4;
+            //     else if (row == 0 && col == TAMANHO-1) quadrante = 3;
+            //     else if (row == TAMANHO-1 && col == 0) quadrante = 2;
+            //     else quadrante = 1;
+            // }
+            
+            int totBacterias = calcularVizinhos(currentGenerationMatrix, row, col, vizinhos, quadrante);
+            
+            if (row == 1 && col == 1) printf("%d\n", totBacterias);
+
+
+            if (currentGenerationMatrix[row][col] == 0 && totBacterias == 3) {
+                nextGenerationMatrix[row][col] = 1;
+            } else if (currentGenerationMatrix[row][col] == 1 && totBacterias < 2) {
+                nextGenerationMatrix[row][col] = 0;
+            } else if (currentGenerationMatrix[row][col] == 1 && totBacterias > 3) {
+                nextGenerationMatrix[row][col] = 0;
+            } else if (currentGenerationMatrix[row][col] == 1 && (totBacterias == 2 || totBacterias == 3)) {
+                nextGenerationMatrix[row][col] = 1;
+            }
+        }
+    }
+}
 
 
 
 void main() {
-    int currentGenerationMatrix[TAMANHO][TAMANHO];
+    int currentGenerationMatrix[3][3] = {
+        {0, 1, 0},
+        {1, 0, 1},
+        {0, 0, 0}
+    };
     int nextGenerationMatrix[TAMANHO][TAMANHO];
 
-    while (1) {
-        fillRandom(currentGenerationMatrix);
-        printMatrix(currentGenerationMatrix);
-        sleep(1);
-    }
+    printMatrix(currentGenerationMatrix);
+    sleep(1);
 
+    proximaGeracao(currentGenerationMatrix, nextGenerationMatrix);
+    printMatrix(nextGenerationMatrix);
 }
