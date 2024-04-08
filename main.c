e1a09506f6028b665d7c567b3e14f479b6d572f4
Luiz Fernando <luizfernandoinfo100@gmail.com>
2024-04-08 18:04:24 -0300

    feat: adicionando args na compilação

--- main.c
+++ main.c
@@ -1,11 +1,31 @@
 #include "game_of_life.h"
 #include "file.h"
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
-void main() {
-    int currentGenerationMatrix[TAMANHO][TAMANHO];
-    int nextGenerationMatrix[TAMANHO][TAMANHO];
+static int delay = 1;
+
+void main(int argc, char* argv[]) {
+    if (argc >= 2) {
+        for (int i = 1; i < argc; i++) {
+            char* chave = strtok(argv[i], "=");
+            char* valor = strtok(NULL, "=");
+
+            if (strcmp(chave, "rows") == 0) {
+                setLinhas(atoi(valor));
+            } else if (strcmp(chave, "cols") == 0) {
+                setColunas(atoi(valor));
+            } else if (strcmp(chave, "sleep") == 0) {
+                delay = atoi(valor);
+            }
+        }
+    }
+
+    int currentGenerationMatrix[getLinhas()][getColunas()];
+    int nextGenerationMatrix[getLinhas()][getColunas()];
     int resultado = 0;
     
     fillRandom(currentGenerationMatrix);
@@ -20,6 +40,6 @@ void main() {
         resultado = proximaGeracao(currentGenerationMatrix, nextGenerationMatrix);
         imprimirEstatisticasCenario();
         
-        sleep(1);
+        sleep(delay);
     }
 }
