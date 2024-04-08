92cabbef83df811f8391b3d580739564b128d897
Luiz Fernando <luizfernandoinfo100@gmail.com>
2024-04-08 18:00:38 -0300

    adicionando função para salvar ultimo cenario em txt

--- file.c
+++ file.c
@@ -1,20 +1,39 @@
 #include "file.h"
 #include <stdio.h>
 
-void cenarioEntrada(int geracaoAtual[TAMANHO][TAMANHO]) {
+void cenarioEntrada(int geracaoAtual[linhas][colunas]) {
     FILE *arquivo;
-    arquivo = fopen("entrada.txt", "r");
+    arquivo = fopen("cenario.txt", "r");
     
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo.");
         return;
     }
     
-    for (int i = 0; i < TAMANHO; i++) {
-        for (int j = 0; j < TAMANHO; j++) {
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas ; j++) {
             fscanf(arquivo, "%1d", &geracaoAtual[i][j]);
         }
     }
     
+    fclose(arquivo);
+}
+
+void salvarCenario(int geracaoAtual[linhas][colunas]) {
+    FILE *arquivo;
+    arquivo = fopen("cenario.txt", "w");
+
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo para escrita.\n");
+        return;
+    }
+
+    for (int i = 0; i < linhas; i++) {
+        for (int j = 0; j < colunas; j++) {
+            fprintf(arquivo, "%d ", geracaoAtual[i][j]);
+        }
+        fprintf(arquivo, "\n");
+    }
+
     fclose(arquivo);
 }
--- file.h
+++ file.h
@@ -1,6 +1,9 @@
+#include "game_of_life.h"
+
 #ifndef FILE_H
 #define FILE_H
 
-void cenarioEntrada(int geracaoAtual[TAMANHO][TAMANHO]);
+void cenarioEntrada(int geracaoAtual[linhas][colunas]);
+void salvarCenario(int geracaoAtual[linhas][colunas]);
 
 #endif
